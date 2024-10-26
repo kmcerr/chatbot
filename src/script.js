da8aa91eba9a94785668c8603f916ca6d929c6a1
kmcerr <dampherz@gmail.com>
2024-10-26 06:42:16 -0400

    Initial commit with basic HTML, CSS, and JS files

---
+++ src/script.js
@@ -0,0 +1,5 @@
+// script.js
+
+document.getElementById("clickButton").addEventListener("click", function() {
+	document.getElementById("message").textContent = "Hello! You've clicked the button!";
+});
